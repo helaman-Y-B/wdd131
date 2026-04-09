067dc93a05c4ae02a5f48423a62f5b358cf4ecca
Helamã Y. Barbour <90625373+helaman-Y-B@users.noreply.github.com>
2026-04-09 01:54:24 +0000

    Finished the large views for the remember page and home page. Also created the script.js file to manage the navigation bar.
    Added some images to add in the home page as hero images.

---
+++ project/scripts/script.js
@@ -0,0 +1,15 @@
+
+const navbutton = document.getElementById('navBtp');
+const navItems = document.querySelectorAll('nav ul li');
+    
+navbutton.addEventListener('click', () => {
+    navItems.forEach(item => {
+        if (item.style.display === 'block') {
+            item.style.display = 'none';
+            navbutton.textContent = '<>';
+        } else {
+            item.style.display = 'block';
+            navbutton.textContent = 'X';
+        }
+    });
+});
